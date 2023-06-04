bb46b5c57828ed7f16ab11dff65bda0b6e2f62c1
William Ferreira <ferreirawill94@gmail.com>
2023-06-04 17:21:42 -0300

    all tests fixed

--- src/api/test_main.py
+++ src/api/test_main.py
@@ -3,20 +3,41 @@ from src.api.main import app
 
 client = TestClient(app)
 
+default_data = {
+    "lastNumbersStored": {"a": 1, "b": 2},
+    "sum": 3,
+    "subtract": -1,
+    "multiply": 2,
+    "divide": 0.5,
+    "power": 1,
+}
+
 def test_get_data():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"responsibility": "Placeholder"}
+    assert response.json() == default_data
 
 def test_post_data():
-    response = client.post("/", json={"responsibility": "Test"})
+    response = client.post("/", json={"a": 3,"b":4})
     assert response.status_code == 201
-    assert response.json() == {"responsibility": "Test"}
+    assert response.json() == {"lastNumbersStored": {"a": 3,"b": 4},
+                                "sum": 7,
+                                "subtract": 1,
+                                "multiply": 12,
+                                "divide": 0.75,
+                                "power": 81
+                                }
 
 def test_put_data():
-    response = client.put("/", json={"responsibility": "Test"})
-    assert response.status_code == 200
-    assert response.json() == {"responsibility": "Test"}
+    response = client.post("/", json={"a": 3,"b":4})
+    assert response.status_code == 201
+    assert response.json() == {"lastNumbersStored": {"a": 3,"b": 4},
+                                "sum": 7,
+                                "subtract": 1,
+                                "multiply": 12,
+                                "divide": 0.75,
+                                "power": 81
+                                }
 
 def test_delete_data():
     response = client.delete("/")
